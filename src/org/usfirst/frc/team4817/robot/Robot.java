550be8f3200b1aa8a6e03fd9a92cf009f1d6ac84
Alan Chang <tcode2k16@gmail.com>
2018-01-29 19:37:05 +0800

    vision tracking

--- src/org/usfirst/frc/team4817/robot/Robot.java
+++ src/org/usfirst/frc/team4817/robot/Robot.java
@@ -1,6 +1,7 @@
 
 package org.usfirst.frc.team4817.robot;
 
+import org.usfirst.frc.team4817.robot.RobotMap;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Command;
@@ -11,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team4817.robot.commands.ExampleCommand;
+import org.usfirst.frc.team4817.robot.commands.GripPipeline;
 import org.usfirst.frc.team4817.robot.subsystems.Arm;
 import org.usfirst.frc.team4817.robot.subsystems.Drive;
 import org.usfirst.frc.team4817.robot.subsystems.ExampleSubsystem;
@@ -42,6 +44,11 @@ public class Robot extends IterativeRobot {
 	//climber
 	public static final Climber climber = new Climber();
 
+	//vision
+	private VisionThread visionThread;
+	private ArrayList<Line> visionOutput;
+	private final Object imgLock = new Object();
+
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
@@ -55,6 +62,19 @@ public class Robot extends IterativeRobot {
 		chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+    camera.setResolution(RobotMap.imageWidth, RobotMap.imageHeight);
+    
+    visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
+        if (!pipeline.filterLinesOutput().isEmpty()) {
+            ArrayList<Line> output = pipeline.filterLinesOutput();
+            synchronized (imgLock) {
+                visionOutput = output;
+            }
+        }
+    });
+    visionThread.start();
 	}
 
 	/**
